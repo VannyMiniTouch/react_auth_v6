3546d8a970d91744c34eecacf4e47748792d0114
VannyMiniTouch <VannyMiniTouch@gmail.com>
2023-06-27 19:03:48 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+  NavLink,
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+const AuthContext = React.createContext(null);
 
-function App() {
+const Navigation = () => {
+  const { token, onLogout } = useAuth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      <NavLink to="/admin">Admin</NavLink>
+
+      {token && (
+        <button type="button" onClick={onLogout}>
+          Sign Out
+        </button>
+      )}
+    </nav>
   );
-}
+};
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <h1>React Router</h1>
+
+      <Navigation />
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </AuthProvider>
+  );
+};
+
+const fakeAuth = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve("2342f2f1d131rf12"), 250);
+  });
+
+const NoMatch = () => {
+  return (
+    <>
+      <h1>Not found</h1>
+    </>
+  );
+};
+
+const Home = () => {
+  const { onLogin } = useAuth();
+
+  return (
+    <>
+      <h2>Home (Public)</h2>
+
+      <button type="button" onClick={onLogin}>
+        Sign In
+      </button>
+    </>
+  );
+};
+
+const Dashboard = () => {
+  const { token } = useAuth();
+
+  return (
+    <>
+      <h2>Dashboard (Protected)</h2>
+
+      <div>Authenticated as {token}</div>
+    </>
+  );
+};
+const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [token, setToken] = React.useState(null);
+
+  const handleLogin = async () => {
+    const token = await fakeAuth();
+
+    setToken(token);
+    const origin = location.state?.from?.pathname || "/dashboard";
+    navigate(origin);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+
+  const value = {
+    token,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => {
+  return React.useContext(AuthContext);
+};
+
+const ProtectedRoute = ({ children }) => {
+  const { token } = useAuth();
+  const location = useLocation();
+
+  if (!token) {
+    return <Navigate to="/home" replace state={{ from: location }} />;
+  }
+
+  return children;
+};
+const Admin = () => {
+  return (
+    <>
+      <h2>Admin (Protected)</h2>
+    </>
+  );
+};
 
 export default App;
